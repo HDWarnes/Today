4d9abdf37c75dfe2c708b418490f6e56c1e1bba9
Hamish Warnes <hdwarnes@gmail.com>
2022-05-02 19:27:17 +0200

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Hamish Warnes on 2022/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
