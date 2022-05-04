476f764d5ae763dac0dbad386de2f27ee9e5c41b
Hamish Warnes <hdwarnes@gmail.com>
2022-05-04 22:00:10 +0200

    making reminders identifiable and updating them

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -8,26 +8,24 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
-   
+    
     var dataSource: DataSource!
-
+    var reminders: [Reminder] = Reminder.sampleData
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
         
         let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
         
-        dataSource = DataSource(collectionView: collectionView) {(collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String)in
+        dataSource = DataSource(collectionView: collectionView) {(collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Reminder.ID)in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
-
+            
         }
         
-        var snapshot = Snapshot()
-        snapshot.appendSections([0])
-        snapshot.appendItems(Reminder.sampleData.map{$0.title})
-        dataSource.apply(snapshot)
+        updateSnapshot()
         
         collectionView.dataSource = dataSource
     }
@@ -38,7 +36,7 @@ class ReminderListViewController: UICollectionViewController {
         listConfiguration.backgroundColor = .clear
         return UICollectionViewCompositionalLayout.list(using: listConfiguration)
     }
-
-
+    
+    
 }
 
---
+++ Today/ReminderDoneButton.swift
@@ -0,0 +1,13 @@
+//
+//  ReminderDoneButton.swift
+//  Today
+//
+//  Created by Hamish Warnes on 2022/05/04.
+//
+
+import UIKit
+
+class ReminderDoneButton: UIButton{
+    var id: Reminder.ID?
+    
+}
